8496dee32b940a3eae9f8549a6e566298bc99f92
Aryan Tyagi <aryan.tyagi@huulke.com>
2024-11-10 13:08:14 +0530

    added ApiResponses and ApiError

--- src/app.js
+++ src/app.js
@@ -3,15 +3,14 @@ const cors = require('cors');
 
 const app = express();
 app.use(
-    cors({
-        origin: process.env.CORS_ORIGIN,
-        credentials: true
-    })
-)
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 
-app.use(express.json({limit:"16kb"})) //for JSON Body
-app.use(express.urlencoded({extended:true, limit:"16kb"})) // FORM DATA
-app.use(express.static("public")) // FOr static files like Images, CSS etc
+app.use(express.json({ limit: '16kb' })); //for JSON Body
+app.use(express.urlencoded({ extended: true, limit: '16kb' })); // FORM DATA
+app.use(express.static('public')); // FOr static files like Images, CSS etc
 
-
-module.exports =  { app }
+module.exports = { app };
--- src/server.js
+++ src/server.js
@@ -1,12 +1,11 @@
 const { app } = require('./app.js');
 require('dotenv').config();
 
-
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 const DBConnection = require('./config/db.js');
 DBConnection();
 
-app.listen(PORT,()=>{
-    console.log(`Server Started on PORT: ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Server Started on PORT: ${PORT}`);
+});
